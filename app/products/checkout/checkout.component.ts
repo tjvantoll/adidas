ce2b166d34379c0f18e65c4491398beab2b9aed6
TJ VanToll <tj.vantoll@gmail.com>
2017-08-31 08:20:43 -0400

    Fixing checkout display

--- app/products/checkout/checkout.component.ts
+++ app/products/checkout/checkout.component.ts
@@ -7,44 +7,42 @@ import { Component } from "@angular/core";
 
 <ActionBar class="action-bar" title="Checkout"></ActionBar>
 
-<ScrollView>
-    <StackLayout class="page">
-        <Label text="Please complete the following information to submit your order." class="m-10 m-b-20" fontSize="16" textWrap="true"></Label>
-        <RadDataForm tkExampleTitle tkToggleNavButton [source]="person">
-            <TKEntityProperty tkDataFormProperty
-                name="name"
-                displayName="Name:"
-                index="0">
-            <TKNonEmptyValidator tkEntityPropertyValidators errorMessage="Please provide a name."></TKNonEmptyValidator>
-            </TKEntityProperty>
-            <TKEntityProperty tkDataFormProperty
-                name="address"
-                displayName="Address:"
-                index="1">
-            </TKEntityProperty>
-            <TKEntityProperty tkDataFormProperty
-                name="card"
-                displayName="Card number:"
-                index="2">
-            </TKEntityProperty>
-            <TKEntityProperty tkDataFormProperty
-                name="expirationMonth"
-                displayName="Exp. month:"
-                valuesProvider="01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12"
-                index="3">
-                <TKPropertyEditor tkEntityPropertyEditor type="Picker"></TKPropertyEditor>
-            </TKEntityProperty>
-            <TKEntityProperty tkDataFormProperty
-                name="expirationYear"
-                displayName="Exp. year:"
-                valuesProvider="2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024"
-                index="4">
-                <TKPropertyEditor tkEntityPropertyEditor type="Picker"></TKPropertyEditor>
-            </TKEntityProperty>
-        </RadDataForm>
-        <Button class="btn" text="Submit order"></Button>
-    </StackLayout>
-</ScrollView>
+<GridLayout class="page" rows="auto, *, auto">
+    <Label row="0" class="p-10 m-b-10" text="Please complete the following information"></Label>
+    <RadDataForm row="1" tkExampleTitle tkToggleNavButton [source]="person">
+        <TKEntityProperty tkDataFormProperty
+            name="name"
+            displayName="Name:"
+            index="0">
+        <TKNonEmptyValidator tkEntityPropertyValidators errorMessage="Please provide a name."></TKNonEmptyValidator>
+        </TKEntityProperty>
+        <TKEntityProperty tkDataFormProperty
+            name="address"
+            displayName="Address:"
+            index="1">
+        </TKEntityProperty>
+        <TKEntityProperty tkDataFormProperty
+            name="card"
+            displayName="Card number:"
+            index="2">
+        </TKEntityProperty>
+        <TKEntityProperty tkDataFormProperty
+            name="expirationMonth"
+            displayName="Exp. month:"
+            valuesProvider="01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12"
+            index="3">
+            <TKPropertyEditor tkEntityPropertyEditor type="Picker"></TKPropertyEditor>
+        </TKEntityProperty>
+        <TKEntityProperty tkDataFormProperty
+            name="expirationYear"
+            displayName="Exp. year:"
+            valuesProvider="2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024"
+            index="4">
+            <TKPropertyEditor tkEntityPropertyEditor type="Picker"></TKPropertyEditor>
+        </TKEntityProperty>
+    </RadDataForm>
+    <Button row="2" class="p-5 m-b-10" text="Complete order"></Button>
+</GridLayout>
 `
 })
 export class CheckoutComponent {
